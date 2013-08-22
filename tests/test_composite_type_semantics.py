5210fd1acb92a875527f6c74841f18f8507ee0fa
Iliyas Jorio <inv.jorio@gmail.com>
2013-08-22 17:04:21 +0200

    tests : composites cross refs

--- tests/test_composite_type_semantics.py
+++ tests/test_composite_type_semantics.py
@@ -12,3 +12,21 @@ class TestCompositeTypeSemantics(LDATestCase):
 		self.assertLDAError(semantic.DuplicateDeclaration, self.check, cls=CompositeType,
 			program='Moule = <a:entier, (**)a:chaîne>')
 
+	def test_composite_complex_cross_references(self):
+		self.check(cls=Module, program='''\
+				algorithme
+				lexique
+					Niveau3 = <a: Niveau2>
+					Niveau1 = <a: entier>
+					Niveau2 = <a: tableau Niveau1[0..2, 0..3]>
+					n3 : Niveau3
+				début fin''')
+
+	def test_composite_cyclic_cross_references(self):
+		self.assertLDAError(semantic.UnresolvableTypeAlias, self.check, cls=Module, program='''\
+				algorithme
+				lexique
+					MouleRecursive1 = <m: (**)MouleRecursive2>
+					MouleRecursive2 = <m: MouleRecursive1>
+				début fin''')
+
