6da5efedb0dcb6e03a2c53608845d612f5219d98
Iliyas Jorio <inv.jorio@gmail.com>
2013-08-25 21:13:31 +0200

    test syntaxe : oubli de "lexique"

--- tests/test_friendly_syntax_errors.py
+++ tests/test_friendly_syntax_errors.py
@@ -1,11 +1,22 @@
 from tests.ldatestcase import LDATestCase
 from lda.errors import syntax
+from lda.module import Module
 from lda.statements import While
 
 class TestFriendlySyntaxErrors(LDATestCase):
 	def test_stray_token_in_while_condition(self):
 		self.assertLDAError(syntax.ExpectedItem, self.analyze, cls=While,
 				program='tantque (**)! faire ftant fin')
+	
+	def test_forgot_lexicon_keyword(self):
+		# TODO vérifier qu'il demande "lexique" ! Là il demande "début"
+		self.assertLDAError(syntax.ExpectedKeyword, self.analyze, cls=Module,
+				program='''\
+				algorithme
+					(**)n: entier
+				début
+					n <- 2
+				fin''')
 
 	def test_stray_integer_in_module(self):
 		self.assertLDAError(syntax.ExpectedItem, self.analyze, program="(**)3")
