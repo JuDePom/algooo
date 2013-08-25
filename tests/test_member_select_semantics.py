cd055b672d6382bdde8f044d67bdda6d4ab55642
Iliyas Jorio <inv.jorio@gmail.com>
2013-08-25 19:55:48 +0200

    fix: MemberSelect ne trouvait plus le composite référencé

---
+++ tests/test_member_select_semantics.py
@@ -0,0 +1,16 @@
+from tests.ldatestcase import LDATestCase
+from lda.errors import semantic
+from lda.typedesc import CompositeType
+from lda.module import Module
+
+class TestMemberSelectSemantics(LDATestCase):
+	def test_simple_member_select(self):
+		self.check(cls=Module, program='''\
+				algorithme
+				lexique
+					Moule = <a: entier>
+					m: Moule
+				début
+					m.a <- 3
+				fin''')
+
