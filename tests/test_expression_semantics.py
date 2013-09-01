1cc0f0b085b7be1f5c63abe83ab3a80438fc0def
Iliyas Jorio <inv.jorio@gmail.com>
2013-09-01 22:26:48 +0200

    tests : nouveaux tests divers

--- tests/test_expression_semantics.py
+++ tests/test_expression_semantics.py
@@ -4,14 +4,23 @@ from lda.module import Algorithm
 
 class TestExpressionSemantics(LDATestCase):
 	def test_assignment_must_be_root_node_in_expression(self):
-		self.assertLDAError(semantic.SemanticError, self.check, cls=Algorithm,
-				program='''\
-						algorithme
-						lexique
-							a: entier
-						début
-							si a (**)<- 3 alors
-							fsi
-						fin
-						''')
+		self.assertLDAError(semantic.SemanticError, self.check, program="""\
+					algorithme
+					lexique
+						a: entier
+					début
+						si a (**)<- 3 alors
+						fsi
+					fin""")
+
+	def test_composite_arithmetic(self):
+		self.assertLDAError(semantic.SemanticError, self.check, program="""\
+				algorithme
+				lexique
+					Moule = <>
+					m1: Moule
+					m2: Moule
+				début
+					m1 = m1 (**)+ m2
+				fin""")
 
--- tests/test_friendly_syntax_errors.py
+++ tests/test_friendly_syntax_errors.py
@@ -4,6 +4,9 @@ from lda.statements import While
 
 class TestFriendlySyntaxErrors(LDATestCase):
 	def test_stray_token_in_while_condition(self):
-		self.assertLDAError(syntax.ExpectedItem, self.check, cls=While,
+		self.assertLDAError(syntax.ExpectedItem, self.analyze, cls=While,
 				program='tantque (**)! faire ftant fin')
+
+	def test_stray_integer_in_module(self):
+		self.assertLDAError(syntax.ExpectedItem, self.analyze, program="(**)3")
 
