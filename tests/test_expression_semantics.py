eb5f3f07d867a6df083ce2ec6e3ff60de3e6f71d
Iliyas Jorio <inv.jorio@gmail.com>
2013-09-04 21:34:43 +0200

    tests : nouveaux tests d'affectations illégales

--- tests/test_expression_semantics.py
+++ tests/test_expression_semantics.py
@@ -13,7 +13,7 @@ class TestExpressionSemantics(LDATestCase):
 						fsi
 					fin""")
 
-	def test_composite_arithmetic(self):
+	def test_composite_arithmetic_with_other_composite_of_same_type(self):
 		self.assertLDAError(semantic.SemanticError, self.check, program="""\
 				algorithme
 				lexique
@@ -24,3 +24,47 @@ class TestExpressionSemantics(LDATestCase):
 					m1 = m1 (**)+ m2
 				fin""")
 
+	def test_composite_arithmetic_with_itself(self):
+		self.assertLDAError(semantic.SemanticError, self.check, program="""\
+				algorithme
+				lexique
+					Moule = <>
+					m1: Moule
+				début
+					m1 = m1 (**)+ m1
+				fin""")
+
+	def test_reassign_composite_alias_to_other_composite_alias(self):
+		self.assertLDAError(semantic.TypeError, self.check, program="""\
+				algorithme
+				lexique
+					Moule = <>
+					Huitre = <>
+				début
+					Moule (**)<- Huitre
+				fin""")
+
+	def test_reassign_composite_alias_to_itself(self):
+		self.assertLDAError(semantic.TypeError, self.check, program="""\
+				algorithme
+				lexique
+					Moule = <>
+				début
+					Moule (**)<- Moule
+				fin""")
+
+	def test_reassign_function(self):
+		self.assertLDAError(semantic.TypeError, self.check, program="""\
+				fonction f() début fin
+				fonction g() début fin
+				algorithme
+				début
+					f (**)<- g
+				fin""")
+
+	def test_assign_to_literal(self):
+		self.assertLDAError(semantic.TypeError, self.check, program="""\
+				algorithme
+				début
+					3 (**)<- 4
+				fin""")
