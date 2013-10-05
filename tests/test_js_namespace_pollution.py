b7eb2d40c8c6ca0ef749d7f97512bfed46cb6a67
Iliyas Jorio <inv.jorio@gmail.com>
2013-10-05 19:49:15 +0200

    test pollution namespace JS par variable globale

--- tests/test_js_namespace_pollution.py
+++ tests/test_js_namespace_pollution.py
@@ -4,12 +4,12 @@ class TestJSNamespacePollution(ldatestcase.LDATestCase):
 	def test_js_global_namespace_pollution(self):
 		# these must not pollute the global namespace
 		extra1 = ''.join(
-				"if (typeof {0} !== 'undefined') throw new Error('{0} should NOT be defined')\n".format(x)
+				"if (typeof {0} !== 'undefined') throw new Error('{0} should NOT be defined');\n".format(x)
 				for x in ("$a", "$b", "$c", "$x", "$y", "$z", "$M", "$f",
 					"P.$a", "P.$b", "P.$c", "P.$x", "P.$y"))
 		# these MUST be in P
 		extra2 = ''.join(
-				"if (typeof {0} === 'undefined') throw new Error('{0} SHOULD be defined')\n".format(x)
+				"if (typeof {0} === 'undefined') throw new Error('{0} SHOULD be defined');\n".format(x)
 				for x in ("P.$f", "P.main", "P.$M", "P.$z"))
 		self.jseval(extracode="P.main();\n"+extra1+extra2, program="""\
 				lexique
@@ -20,6 +20,7 @@ class TestJSNamespacePollution(ldatestcase.LDATestCase):
 				lexique
 					b: entier
 				début
+					z <- 789
 				fin
 
 				algorithme
@@ -27,6 +28,7 @@ class TestJSNamespacePollution(ldatestcase.LDATestCase):
 					c: entier
 				début
 					c <- 123
+					z <- 456
 					f(c)
 				fin""")
 
